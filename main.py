d992e68d2aef7207c0ed6251112130453dcfd1bc
Soumyajit-io <soumyajit010805@gmail.com>
2025-11-15 14:10:04 +0530

    Added history and separate the agent

--- main.py
+++ main.py
@@ -1,43 +1,30 @@
-from langchain_community.tools import ShellTool
-from langchain.agents import create_react_agent, AgentExecutor
-from langchain import hub
-from langchain_core.tools import tool
-from dotenv import load_dotenv
-from langchain_core.prompts import load_prompt
-from Tools.html_opener import  open_in_browser
-from Tools.basic import write_content,create_file,create_folder,read_file
+from agent import create_agent
+from langchain_core.messages import HumanMessage,AIMessage
 
-from langchain.prompts import PromptTemplate
-load_dotenv()
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_openai import ChatOpenAI
+agent = create_agent()
 
+history =[]
+def main():
+   print("====Welcome to AI Coding Assistant====")
+   while True : 
+      user_input = input("🧑 You: ")
 
-ques = input("🤖 Enter what to create : ")
-prompt= load_prompt('prompt.json')
- 
+      
+      if user_input.lower() in ["exit", "quit"]:
+            print("Goodbye! Developed by SoumyajiT")
+            break
+      
+      # store
+      history.append(HumanMessage(user_input))
+      
+      # llm calling
+      response = agent.invoke({'input':history})
 
+      # print
+      print(f"🤖: {response['output']}")
 
-llm2 = ChatOpenAI(model="gpt-4o-mini")
+      # store
+      history.append(AIMessage(response['output']))
 
-agent = create_react_agent(
-   llm = llm2,
-   tools=[create_file,
-          write_content,
-          read_file,
-          create_folder,
-          open_in_browser],
-   prompt=prompt
-)
-agent_executer = AgentExecutor(
-   agent = agent,
-   tools=[create_file,
-          write_content,
-          read_file,
-          create_folder,
-          open_in_browser],
-   verbose= True,
-   handle_parsing_errors=True
-)
-response = agent_executer.invoke({'input':ques})
-print(response['output'])
+if __name__ == '__main__':
+   main()
